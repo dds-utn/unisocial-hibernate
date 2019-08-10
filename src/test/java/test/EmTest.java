23ed7f8d0f2e59fde68422b99d9c2bf68b5ed394
Gaston Prieto <gastonprietoutn@gmail.com>
2019-08-10 09:26:50 -0300

    comento unos tests

--- src/test/java/test/EmTest.java
+++ src/test/java/test/EmTest.java
@@ -10,45 +10,37 @@ import java.time.LocalDate;
 
 public class EmTest{
 
-    @Test
-    public void persistir1UsuarioTest(){
-        Usuario usuario = new Usuario();
-        usuario.setNombre("Eze");
-        usuario.setApellido("Escobar");
-        usuario.setTelefono(44889966);
-        usuario.setLegajo(1527778);
-        usuario.setFechaDeNacimiento(LocalDate.of(1995,10,14));
-
-        EntityManagerHelper.beginTransaction();
-        EntityManagerHelper.getEntityManager().persist(usuario);
-        EntityManagerHelper.commit();
-    }
-
-    @Test
-    public void recuperandoAEze(){
-        Usuario eze = (Usuario) EntityManagerHelper.createQuery("from Usuario where nombre = 'Eze'").getSingleResult();
-        Assert.assertEquals("Eze", eze.getNombre());
-    }
-
-    @Test
-    public void persistir2Aporte(){
-        Topico topicoApunte = new Topico();
-        topicoApunte.setNombre("Apunte");
-        topicoApunte.setDescripcion("Colaboración de un apunte");
-
-        EntityManagerHelper.beginTransaction();
-        EntityManagerHelper.getEntityManager().persist(topicoApunte);
-        EntityManagerHelper.commit();
-
-        Aporte unAporte = new Aporte();
-        unAporte.setTopico(topicoApunte);
-        Usuario eze = (Usuario) EntityManagerHelper.createQuery("from Usuario where nombre = 'Eze'").getSingleResult();
-        unAporte.setUsuario(eze);
-        unAporte.setNombre("Apunte de diseño");
-        unAporte.setDescripcion("Taller ORM");
-
-        EntityManagerHelper.beginTransaction();
-        EntityManagerHelper.getEntityManager().persist(unAporte);
-        EntityManagerHelper.commit();
-    }
+	/*
+	 * @Test public void persistir1UsuarioTest(){ Usuario usuario = new Usuario();
+	 * usuario.setNombre("Eze"); usuario.setApellido("Escobar");
+	 * usuario.setTelefono(44889966); usuario.setLegajo(1527778);
+	 * usuario.setFechaDeNacimiento(LocalDate.of(1995,10,14));
+	 * 
+	 * EntityManagerHelper.beginTransaction();
+	 * EntityManagerHelper.getEntityManager().persist(usuario);
+	 * EntityManagerHelper.commit(); }
+	 * 
+	 * @Test public void recuperandoAEze(){ Usuario eze = (Usuario)
+	 * EntityManagerHelper.createQuery("from Usuario where nombre = 'Eze'").
+	 * getSingleResult(); Assert.assertEquals("Eze", eze.getNombre()); }
+	 * 
+	 * @Test public void persistir2Aporte(){ Topico topicoApunte = new Topico();
+	 * topicoApunte.setNombre("Apunte");
+	 * topicoApunte.setDescripcion("Colaboración de un apunte");
+	 * 
+	 * EntityManagerHelper.beginTransaction();
+	 * EntityManagerHelper.getEntityManager().persist(topicoApunte);
+	 * EntityManagerHelper.commit();
+	 * 
+	 * Aporte unAporte = new Aporte(); unAporte.setTopico(topicoApunte); Usuario eze
+	 * = (Usuario)
+	 * EntityManagerHelper.createQuery("from Usuario where nombre = 'Eze'").
+	 * getSingleResult(); unAporte.setUsuario(eze);
+	 * unAporte.setNombre("Apunte de diseño");
+	 * unAporte.setDescripcion("Taller ORM");
+	 * 
+	 * EntityManagerHelper.beginTransaction();
+	 * EntityManagerHelper.getEntityManager().persist(unAporte);
+	 * EntityManagerHelper.commit(); }
+	 */
 }
